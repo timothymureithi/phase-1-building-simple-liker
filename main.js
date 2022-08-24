df8c7cd9a25b9b00de51c060b79b0ef9f543bf57
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-24 17:44:41 +0300

    completed lab

--- main.js
+++ main.js
@@ -3,9 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modalDiv = document.querySelector("#modal");
+modalDiv.classList.add("hidden");
 
 
+const clickHeart = (event) =>{
+  const heart = event.target
+  heart.innerText = EMPTY_HEART
+  mimicServerCall()
+    .then( res => {
+      if(heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.classList.add("activated-heart")
+      }
+      else{
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove("activated-heart")
+      }
+      
+      
+    })
+    .catch(err => {
+      modalDiv.classList.remove("hidden");
+      modalDiv.querySelector("#modal-message").textContent = err;
+      setTimeout( () => {
+        modalDiv.classList.add("hidden")
+      }, 3000)
+    }) 
+  
+}
 
+
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach( heart => {
+  heart.addEventListener("click", clickHeart)
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
